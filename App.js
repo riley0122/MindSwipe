a49139ee1a69187a59937f5430a29d484d10cf69
LucasAmmer <lucasammerlaan123@gmail.com>
2023-12-05 16:09:36 +0100

    Created Welcome screen and changed some settings

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+function Hello() {
+  return (
+    <View>
+      <Text style={{ fontSize: 20, color: "white", fontWeight: "bold" }}>
+        Welcome to MindSwipe!
+      </Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        <Hello />
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#001F3F",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
   },
 });
